d12bda58d09897e006e48db4435486847affafed
Lennart Rolland <lennart@vitawater.no>
2016-01-15 22:57:11 +0100

    added icon design SVG

---
+++ lib/comms/messages/QueryMessage.cpp
@@ -0,0 +1,7 @@
+#include "QueryMessage.hpp"
+
+QueryMessage::QueryMessage()
+{
+
+}
+
---
+++ lib/comms/messages/QueryMessage.hpp
@@ -0,0 +1,15 @@
+#ifndef QUERYMESSAGE_HPP
+#define QUERYMESSAGE_HPP
+
+
+class QueryMessage
+{
+	public:
+		QueryMessage();
+
+	signals:
+
+	public slots:
+};
+
+#endif // QUERYMESSAGE_HPP
