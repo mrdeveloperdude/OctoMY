bb21a7d97af97c3176779c1d02f0af2a34180dd3
Lennart Rolland <lennart@vitawater.no>
2016-02-24 04:08:51 +0100

    added hexedit files that were missing

---
+++ lib/widgets/hexedit/QHexEditDataDevice.hpp
@@ -0,0 +1,25 @@
+#ifndef QHEXEDITDATADEVICE_H
+#define QHEXEDITDATADEVICE_H
+
+#include <QtCore>
+#include "QHexEditData.hpp"
+#include "QHexEditDataReader.hpp"
+#include "QHexEditDataWriter.hpp"
+
+class QHexEditDataDevice : public QIODevice
+{
+    public:
+        QHexEditDataDevice(QHexEditData* hexeditdata);
+        virtual qint64 size();
+
+    protected:
+        qint64 readData(char *data, qint64 maxlen);
+        qint64 writeData(const char *data, qint64 len);
+
+    private:
+        QHexEditData* _hexeditdata;
+        QHexEditDataReader* _reader;
+        QHexEditDataWriter* _writer;
+};
+
+#endif // QHEXEDITDATADEVICE_H
