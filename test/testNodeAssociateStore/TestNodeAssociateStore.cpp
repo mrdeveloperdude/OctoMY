f0acb28241e8e0687aef911597dab2c6ea12bb9d
Lennart Rolland <lennart@octomy.org>
2016-07-29 00:15:11 +0200

    added test for NodeAssociateStore

---
+++ test/testNodeAssociateStore/TestNodeAssociateStore.cpp
@@ -0,0 +1,9 @@
+#include "TestNodeAssociateStore.hpp"
+
+// YOU NEED THIS: http://doc.qt.io/qt-5/qtest.html
+void TestNodeAssociateStore::test(){
+}
+
+
+
+QTEST_MAIN(TestNodeAssociateStore)
---
+++ test/testNodeAssociateStore/TestNodeAssociateStore.hpp
@@ -0,0 +1,16 @@
+#ifndef TESTNODEASSOCIATESTORE_HPP
+#define TESTNODEASSOCIATESTORE_HPP
+
+#include <QTest>
+
+
+class TestNodeAssociateStore:public QObject{
+		Q_OBJECT
+
+	private slots:
+		void test();
+
+};
+
+
+#endif // TESTNODEASSOCIATESTORE_HPP
