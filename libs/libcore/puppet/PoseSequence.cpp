161fc0bc697ea42e9264b01a48d092e2b0aaca90
Lennart Rolland <lennart@octomy.org>
2016-12-20 23:38:06 +0100

    added Pose Sequence

---
+++ libs/libcore/puppet/PoseSequence.cpp
@@ -0,0 +1,6 @@
+#include "PoseSequence.hpp"
+
+PoseSequence::PoseSequence()
+{
+
+}
---
+++ libs/libcore/puppet/PoseSequence.hpp
@@ -0,0 +1,11 @@
+#ifndef POSESEQUENCE_HPP
+#define POSESEQUENCE_HPP
+
+
+class PoseSequence
+{
+	public:
+		PoseSequence();
+};
+
+#endif // POSESEQUENCE_HPP
