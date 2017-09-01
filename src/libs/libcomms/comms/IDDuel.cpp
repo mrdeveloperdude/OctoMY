1ff5ecaccbce6881f56bc0a7f2d7319f68802149
Lennart Rolland <lennart@octomy.org>
2017-09-01 22:08:29 +0200

    Added ID Duel class

--- src/libs/libcomms/comms/IDDuel.cpp
+++ src/libs/libcomms/comms/IDDuel.cpp
@@ -1,6 +1,23 @@
 #include "IDDuel.hpp"
 
-IDDuel::IDDuel()
+#include <QCryptographicHash>
+#include <QByteArray>
+
+IDDuel::IDDuel(QString idA, QString idB)
+	: mIDA(idA)
+	, mIDB(idB)
 {
 
+}
+
+
+bool IDDuel::duel(QString key)
+{
+		   QCryptographicHash hashA(QCryptographicHash::Sha1);
+		   hashA.addData(key+mIDA);
+		   QByteArray rawA=hashA.result();
+		   QCryptographicHash hashB(QCryptographicHash::Sha1);
+		   hashB.addData(key+mIDB);
+		   QByteArray rawB=hashB.result();
+		   return rawA>rawB;
 }
--- src/libs/libcomms/comms/IDDuel.hpp
+++ src/libs/libcomms/comms/IDDuel.hpp
@@ -1,11 +1,18 @@
 #ifndef IDDUEL_HPP
 #define IDDUEL_HPP
 
+#include <QString>
 
 class IDDuel
 {
-	public:
-		IDDuel();
+private:
+	QString mIDA;
+	QString mIDB;
+public:
+	IDDuel(QString idA, QString idB);
+public:
+
+	bool duel(QString key);
 };
 
 #endif // IDDUEL_HPP
