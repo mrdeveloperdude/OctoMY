d71b843c83b8b16f35ebbcf50c099d5edd46fce1
Lennart Rolland <lennart@octomy.org>
2017-09-16 00:16:34 +0200

    updated ci build script to copy overrides instead of heredocking them

--- integration/scripts/build_ci.sh
+++ integration/scripts/build_ci.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
 qmk="/home/lennart/Qt/5.8/gcc_64/bin/qmake"
-overrides="pri/local_overrides.pri"
+overrides_src="integrations/travis_ci/overrides.pri"
+overrides_dst="pri/local_overrides.pri"
 pro="OctoMY.pro"
 bdir="build"
-cat << EOF > $overrides
-
-#DEFINES += USE_DOCS
-DEFINES += USE_TESTS_BASIC
-#DEFINES += USE_TESTS_WEB
-#DEFINES += USE_TESTS_STRESS
-#DEFINES += USE_TESTS_SELECTED
-#DEFINES += USE_QT3D
-#DEFINES += EXTERNAL_LIB_ESPEAK
-#DEFINES += EXTERNAL_LIB_OPENCV
-#DEFINES += EXTERNAL_LIB_OPENCL
-DEFINES += USE_ARDUINO
-#DEFINES += USE_STATUS
-
-CONFIG += silent
-
-EOF
-
-
+cp -a "$overrides_src" "$overrides_dst"
+cat "$overrides_dst"
 mkdir -p "$bdir"
 pushd "$bdir"
 "$qmk" "../$pro"
