81d1f58e4e15c27a1f80c6a58312f051d98b2b6f
Lennart Rolland <lennart@octomy.org>
2017-09-16 00:26:21 +0200

    fixed bugs in build_ci.sh

--- integration/scripts/build_ci.sh
+++ integration/scripts/build_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 qmk="/home/lennart/Qt/5.8/gcc_64/bin/qmake"
-overrides_src="integrations/travis_ci/overrides.pri"
+overrides_src="integration/travis_ci/overrides.pri"
 overrides_dst="pri/local_overrides.pri"
 pro="OctoMY.pro"
 bdir="build"
