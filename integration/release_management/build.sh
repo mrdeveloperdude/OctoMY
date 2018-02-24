85144d69225229bc69bfe4882bf4a8233b4b776c
Lennart Rolland <lennart@octomy.org>
2018-02-24 15:54:49 +0100

    fixed release management for debian package and debian test run

--- integration/release_management/build.sh
+++ integration/release_management/build.sh
@@ -2,6 +2,14 @@
 
 TARGET_PLATFORMS=""
 TARGET_PLATFORMS+="Dockerfile.qt5.10.0_static_ubuntu_amd64"
+
+TARGET_PACKAGES=""
+TARGET_PACKAGES+=" agent"
+TARGET_PACKAGES+=" remote"
+TARGET_PACKAGES+=" hub"
+TARGET_PACKAGES+=" zoo"
+
+
 OCTOMY="Dockerfile.octomy"
 DEB="Dockerfile.deb_file"
 DEBTEST="Dockerfile.deb_test"
@@ -31,35 +39,33 @@ do
 		exit
 	fi
 	echo "-------------------------------- OCTOMY: '$oc_target'"
-	docker build -f "$OCTOMY" --build-arg "oc_base=$qt_target" -t "$oc_target" $cache .
+	docker build -f "$OCTOMY"	--build-arg "oc_base=$qt_target"		-t "$oc_target" $cache .
 	if [ ! $? -eq 0 ]; then
 		echo "Fail Octomy build"
 		exit
 	fi
 
 	echo "----------------------------------- DEB: '$deb_target'"
-	docker build -f "$DEB"    --build-arg "deb_base=$oc_target" -t "$deb_target" --no-cache .
+	docker build -f "$DEB"		--build-arg "deb_base=$oc_target"		-t "$deb_target" .
 	if [ ! $? -eq 0 ]; then
 		echo "Fail deb build"
 		exit
 	fi
 
 	echo "----------------------------------- DEBTEST BUILD: '$debtest_target'"
-	docker build -f "$DEBTEST"    --build-arg "debtest_base=$deb_target" -t "$debtest_target" .
+	docker build -f "$DEBTEST"	--build-arg "debtest_base=$deb_target"	-t "$debtest_target" .
 	if [ ! $? -eq 0 ]; then
 		echo "Fail debtest build"
 		exit
 	fi
 
+
 	echo "----------------------------------- DEBTEST RUN: '$debtest_target'"
-	docker run -p 5900 "$debtest_target" x11vnc -forever -nopw -ncache_cr -ncache 10 -create -display :0
+	#docker run -p 5900 "$debtest_target" x11vnc -forever -nopw -ncache_cr -ncache 10 -display :99 -auth guess
+	docker run -p 5900:5900 "$debtest_target" -geometry 1024x768
 	if [ ! $? -eq 0 ]; then
 		echo "Fail debtest start"
 		exit
 	fi
-
-	#cont=$(docker create "$deb_target")
-	#docker cp "$cont:/root/deb/agent.deb" .
-	#docker rm "$cont"
 done
 
