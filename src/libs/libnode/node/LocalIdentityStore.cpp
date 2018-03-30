afb6b176ae0b75cc311c1556e83b0cf8cd67aa28
Lennart Rolland <lennart@octomy.org>
2018-03-30 21:04:27 +0200

    Separated local identity store into its own class

---
+++ src/libs/libnode/node/LocalIdentityStore.cpp
@@ -0,0 +1,6 @@
+#include "LocalIdentityStore.hpp"
+
+LocalIdentityStore::LocalIdentityStore()
+{
+
+}
---
+++ src/libs/libnode/node/LocalIdentityStore.hpp
@@ -0,0 +1,11 @@
+#ifndef LOCALIDENTITYSTORE_HPP
+#define LOCALIDENTITYSTORE_HPP
+
+
+class LocalIdentityStore
+{
+	public:
+		LocalIdentityStore();
+};
+
+#endif // LOCALIDENTITYSTORE_HPP
