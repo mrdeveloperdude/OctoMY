407da05bfe9b02ae01eb9352eade035277f4fe2b
Lennart Rolland <lennart@octomy.org>
2018-10-16 00:21:59 +0200

    Added qmake test to test docker build

---
+++ integration/docker/qmake_test/main.cpp
@@ -0,0 +1,24 @@
+#include "Widget.hpp"
+#include <QApplication>
+
+
+#include <QMediaPlayer>
+#include <QVideoWidget>
+#include <QMediaPlaylist>
+
+
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	QMediaPlayer *player = new QMediaPlayer;
+	QMediaPlaylist *playlist = new QMediaPlaylist(player);
+	playlist->addMedia(QMediaContent(QUrl::fromLocalFile("/home/lennart/Desktop/vid.mp4")));
+	playlist->addMedia(QMediaContent(QUrl::fromLocalFile("/home/lennart/Desktop/vid.mp4")));
+	QVideoWidget *videoWidget = new QVideoWidget();
+	player->setVideoOutput(videoWidget);
+	videoWidget->show();
+	playlist->setCurrentIndex(1);
+	player->play();
+	return a.exec();
+}
