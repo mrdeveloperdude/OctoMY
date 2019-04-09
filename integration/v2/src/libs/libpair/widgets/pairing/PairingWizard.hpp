457b893fc1671e687319016e2fbd7bc20dbca99a
Lennart Rolland <lennart@octomy.org>
2019-04-09 19:53:46 +0200

    Adapted pairing wizarc to use gender generator

--- integration/v2/src/libs/libpair/widgets/pairing/PairingWizard.hpp
+++ integration/v2/src/libs/libpair/widgets/pairing/PairingWizard.hpp
@@ -24,6 +24,22 @@ class QAbstractButton;
 class AddressBook;
 class LocalAddressList;
 
+
+
+/**
+ * @brief The PairingWizard class is a widget that is used by the nodes' UI when
+ * they want input from user on what trust should be placed on each associate
+ * that has been found through the discovery process.
+ *
+ * The widget has 3 main components:
+ * + network settings - A widget to select address and port for communicating with associates
+ * + associate list - A list of all associates found through discovery so far
+ * + trust editor - A screen that allows user to tweak trust for one individual associate
+ *
+ * It also has a display of our current birth certificate and initiation of several 2-factor methods
+ *
+ */
+
 class PairingWizard : public QWidget
 {
 	Q_OBJECT
@@ -66,7 +82,6 @@ private slots:
 	void onNetworkSettingsChange(QHostAddress address, quint16 port, bool valid);
 	void onTrustButtonClicked(QAbstractButton *);
 	void onPulsatingTrustTimer();
-
 
 private slots:
 	void on_pushButtonMaybeOnward_clicked();
