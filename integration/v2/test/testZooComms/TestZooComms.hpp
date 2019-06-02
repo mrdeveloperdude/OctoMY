4d3cb366505be8fe1371d03a17c9c37b53c3e018
Lennart Rolland <lennart@octomy.org>
2019-06-02 02:31:27 +0200

    Added zoo comms test

---
+++ integration/v2/test/testZooComms/TestZooComms.hpp
@@ -0,0 +1,18 @@
+#ifndef TESTZOOCOMMS_HPP
+#define TESTZOOCOMMS_HPP
+
+#include "Common_test.hpp"
+
+
+class TestZooComms:public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void test();
+
+};
+
+
+#endif
+// TESTZOOCOMMS_HPP
