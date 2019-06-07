f83be437e3469c687f364da90227b03923ac73fe
Lennart Rolland <lennart@octomy.org>
2019-06-07 03:21:12 +0200

    Added fps calculator

---
+++ src/libs/libutil/utility/graphics/FPSCalculator.cpp
@@ -0,0 +1,37 @@
+#include "FPSCalculator.hpp"
+
+#include "utility/time/HumanTime.hpp"
+#include <QDateTime>
+#include <QDebug>
+
+
+FPSCalculator::FPSCalculator(QString name, quint64 logInterval)
+	: name(name)
+	, logInterval(logInterval)
+	, frameAccumulator(0)
+	, lastTime(0)
+{
+
+}
+
+
+FPSCalculator::~FPSCalculator()
+{
+	frameAccumulator=0;
+	lastTime=0;
+}
+
+
+void FPSCalculator::update()
+{
+	quint64 now=utility::time::currentMsecsSinceEpoch<quint64>();
+	quint64 interval=now-lastTime;
+	frameAccumulator++;
+	if(interval>logInterval) {
+		double fps=frameAccumulator*1000;
+		fps/=interval;
+		qDebug().noquote().nospace()<<"%%%% '"<<name<<"' FPS="<<fps;
+		lastTime=now;
+		frameAccumulator=0;
+	}
+}
